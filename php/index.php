2453a9c7f7fe6d037f8cd3ad06fd27274b51a9ae
Hafiz Zohaib <hafizzuhaib05@gmail.com>
2024-09-10 19:20:11 +0500

    Add faveicons

--- php/index.php
+++ php/index.php
@@ -7,6 +7,11 @@
   <title>Register & Login</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   <link rel="stylesheet" href="../Style sheets/signin.css">
+  <link
+      rel="shortcut icon"
+      href="Assets/Images/favicon.ico"
+      type="image/x-icon"
+    />
 </head>
 
 <body>
